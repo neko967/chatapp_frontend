86554316f870070a4118265a197566d39e1f4ca7
neko967 <113199838+neko967@users.noreply.github.com>
2023-10-18 15:07:56 +0900

    first_commit

---
+++ src/app/page.tsx
@@ -0,0 +1,51 @@
+"use client"
+import { useEffect, useState, useRef } from 'react';
+
+const Home = () => {
+    const [message, setMessage] = useState("");
+    const [chat, setChat] = useState<string[]>([]);
+    const [room, setRoom] = useState<string>("default");
+    const ws = useRef<WebSocket | null>(null);
+
+    useEffect(() => {
+        if (room) {
+            ws.current = new WebSocket(`ws://localhost:8000/ws/${room}`);
+            ws.current.onmessage = (event) => {
+                setChat((prevChat) => [...prevChat, event.data]);
+            };
+
+            return () => {
+                if (ws.current) ws.current.close();
+            };
+        }
+    }, [room]);
+
+    const sendMessage = () => {
+        if (ws.current && message) {
+            ws.current.send(message);
+            setMessage("");
+        }
+    };
+
+    return (
+        <div>
+            <input
+                placeholder="Enter room name"
+                value={room}
+                onChange={(e) => setRoom(e.target.value)}
+            />
+            <ul>
+                {chat.map((msg, idx) => (
+                    <li key={idx}>{msg}</li>
+                ))}
+            </ul>
+            <input
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+            />
+            <button onClick={sendMessage}>Send</button>
+        </div>
+    );
+};
+
+export default Home;
